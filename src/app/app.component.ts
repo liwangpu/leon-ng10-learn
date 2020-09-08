315c31f65030e5c2dec317f128d0687a659628c9
Leon Pu <puliwang@cxist.com>
2020-09-08 16:13:10 +0800

    空的ng10项目

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mirror-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+}

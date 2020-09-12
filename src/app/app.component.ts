5883e23056a8bd816ab6f48412b50ce7d840078b
leon <puliwang@cxist.com>
2020-09-12 19:37:12 +0800

    添加两个懒加载模块供学习

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'mirror-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
+    constructor() {
+
+    }
 }

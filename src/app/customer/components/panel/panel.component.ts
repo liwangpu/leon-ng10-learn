a427088fc1b887c0caf190d4f4e9bb8812e6f73d
Leon Pu <puliwang@cxist.com>
2020-09-17 09:32:43 +0800

    添加子类添加基类各个周期调用

--- src/app/customer/components/panel/panel.component.ts
+++ src/app/customer/components/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, SimpleChanges } from '@angular/core';
 import { Logger } from '../../models/logger';
 
 @Component({
@@ -18,6 +18,35 @@ export class PanelComponent extends Logger {
     public key: string = 'panel';
     public constructor() {
         super();
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        super.ngOnChanges(changes);
+    }
+
+    public ngOnInit(): void {
+        super.ngOnInit();
+    }
+
+
+    public ngDoCheck(): void {
+        super.ngDoCheck();
+    }
+
+    public ngAfterContentInit(): void {
+        super.ngAfterContentInit();
+    }
+
+    public ngAfterContentChecked(): void {
+        super.ngAfterContentChecked();
+    }
+
+    public ngAfterViewInit(): void {
+        super.ngAfterViewInit();
+    }
+
+    public ngAfterViewChecked(): void {
+        super.ngAfterViewChecked();
     }
 
     public test(): void {

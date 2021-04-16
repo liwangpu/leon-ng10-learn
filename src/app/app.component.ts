2748cad2f0fa5b0e5968554426fd4fc87bd5d31d
puliwang@cxist.com <puliwang@cxist.com>
2021-04-16 09:33:01 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import hotkeys from 'hotkeys-js';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
-    public constructor() {
-
+    public ngOnDestroy(): void {
+        // hotkeys.unbind('del', 'mytest');
+        hotkeys.deleteScope('app1');
     }
 
     public ngOnInit(): void {
+        hotkeys('del', 'app1', (event, handler) => {
+            event.preventDefault()
+            alert('app pressed F5!')
+        });
 
+        hotkeys.setScope('app1');
     }
 }
--- src/app/customer/components/home/home.component.ts
+++ src/app/customer/components/home/home.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import hotkeys from 'hotkeys-js';
 
 @Component({
     selector: 'customer-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
     public constructor() {
 
     }
 
-    public ngOnInit(): void {
+    public ngOnDestroy(): void {
+        // hotkeys.unbind('del', 'mytest');
+        this.stopHotkeys();
+    }
 
+    public ngOnInit(): void {
+        // this.startHotkeys();
+    }
+
+    public stopHotkeys(): void {
+        hotkeys.deleteScope('customer');
+    }
+
+    public getScope(): void {
+        let sss = hotkeys.getScope();
+        console.log('getScope:', sss);
+    }
+
+    public startHotkeys(): void {
+        hotkeys('del', 'customer', (event, handler) => {
+            // event.preventDefault()
+            alert('customer pressed F5 1')
+        });
+
+        hotkeys.setScope('customer');
+    }
+
+    public startAllHotkeys(): void {
+        hotkeys.setScope('app1');
     }
 }
